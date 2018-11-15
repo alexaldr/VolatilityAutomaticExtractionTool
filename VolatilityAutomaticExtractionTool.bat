f5d8223cb4944596a3d82600d7abb2a76b55dade
Alex Almeida <alex.aldr@gmail.com>
2018-11-15 03:32:39 -0200

    Initial commit

---
+++ VolatilityAutomaticExtractionTool.bat
@@ -0,0 +1,142 @@
+@REM Arquivo criado por Alex Almeida
+@REM alex.aldr@gmail.com
+
+@echo off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+
+title: Extracao Forense Automatica - by Alex Almeida
+
+echo.
+call :colorEcho 0C "       d8888 888      8888888888 Y88b   d88P " & echo.
+call :colorEcho 0C "      d88888 888      888         Y88b d88P  " & echo.
+call :colorEcho 0C "     d88P888 888      888          Y88o88P   " & echo.
+call :colorEcho 0C "    d88P 888 888      8888888       Y888P    " & echo.
+call :colorEcho 0C "   d88P  888 888      888           d888b    " & echo.
+call :colorEcho 0C "  d88P   888 888      888          d88888b   " & echo.
+call :colorEcho 0C " d8888888888 888      888         d88P Y88b  " & echo.
+call :colorEcho 0C "d88P     888 88888888 8888888888 d88P   Y88b " & echo.
+echo.
+call :colorEcho 0F "alex.aldr@gmail.com" & echo.
+echo.
+echo.
+
+set INPUT=
+set /P INPUT=Nome do arquivo de dump: %=%
+echo.
+
+call :colorEcho 0E "Gerando Hash -sha1 e -md5" & echo.
+fciv.exe -add %INPUT% -both > %INPUT%.hash
+echo.
+
+call :colorEcho 0E "Gerando IMAGEINFO (perfil do Sistema Operacional)" & echo.
+volatility.exe -f %INPUT% imageinfo > %INPUT%.imageinfo
+echo.
+
+set INPUTOS=
+set /P INPUTOS=Perfil do sistema operacional: %=%
+echo.
+
+call :colorEcho 0E "Gerando PSLIST (processos em execucao)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% pslist > %INPUT%.pslist
+echo.
+
+call :colorEcho 0E "Gerando PSTREE (arvore de processos)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% pstree > %INPUT%.pstree
+echo.
+
+call :colorEcho 0E "Gerando PSSCAN (processos encerrados anteriormente)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% psscan > %INPUT%.psscan
+echo.
+
+call :colorEcho 0E "Gerando PSXVIEW (processos ocultos)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% psxview > %INPUT%.psxview
+echo.
+
+call :colorEcho 0E "Gerando CONNECTIONS (conexoes abertas)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% connections > %INPUT%.connections
+echo.
+
+call :colorEcho 0E "Gerando CONNSCAN (conexoes encerradas)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% connscan > %INPUT%.connscan
+echo.
+
+call :colorEcho 0E "Gerando DLLLIST (DLLs em execucao)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% dlllist > %INPUT%.dlllist
+echo.
+
+call :colorEcho 0E "Gerando FILESCAN (arquivos abertos anteriormente)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% filescan > %INPUT%.filescan
+echo.
+
+call :colorEcho 0E "Gerando HANDLES (manipulacoes nos processos)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% handles > %INPUT%.handles
+echo.
+
+call :colorEcho 0E "Gerando CMDSCAN (historico do CMD)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% cmdscan > %INPUT%.cmdscan
+echo.
+
+call :colorEcho 0E "Gerando CONSOLES (historico CMD e buffer de exibicao)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% consoles > %INPUT%.consoles
+echo.
+
+call :colorEcho 0E "Gerando MBRPARSER (informacoes do setor MBR)" & echo.
+volatility.exe -f %INPUT% mbrparser > %INPUT%.mbrparser
+echo.
+
+call :colorEcho 0E "Gerando MFTPARSER (informacoes da MFT)" & echo.
+volatility.exe -f %INPUT% mftparser > %INPUT%.mftparser
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+call :colorEcho 0A " .d88888b.  888    d8P  " & echo.
+call :colorEcho 0A "d88P   Y88b 888   d8P   " & echo.
+call :colorEcho 0A "888     888 888  d8P    " & echo.
+call :colorEcho 0A "888     888 888d88K     " & echo.
+call :colorEcho 0A "888     888 8888888b    " & echo.
+call :colorEcho 0A "888     888 888  Y88b   " & echo.
+call :colorEcho 0A "Y88b. .d88P 888   Y88b  " & echo.
+call :colorEcho 0A "  Y88888P   888    Y88b " & echo.
+echo.
+echo.
+echo.
+echo.
+pause
+exit
+
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ VolatilityAutomaticExtractionTool_PID.bat
@@ -0,0 +1,107 @@
+@REM Arquivo criado por Alex Almeida
+@REM alex.aldr@gmail.com
+
+@echo off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+
+title: Extracao Forense Automatica (por PID) - by Alex Almeida
+
+echo.
+call :colorEcho 0C "       d8888 888      8888888888 Y88b   d88P " & echo.
+call :colorEcho 0C "      d88888 888      888         Y88b d88P  " & echo.
+call :colorEcho 0C "     d88P888 888      888          Y88o88P   " & echo.
+call :colorEcho 0C "    d88P 888 888      8888888       Y888P    " & echo.
+call :colorEcho 0C "   d88P  888 888      888           d888b    " & echo.
+call :colorEcho 0C "  d88P   888 888      888          d88888b   " & echo.
+call :colorEcho 0C " d8888888888 888      888         d88P Y88b  " & echo.
+call :colorEcho 0C "d88P     888 88888888 8888888888 d88P   Y88b " & echo.
+echo.
+call :colorEcho 0F "alex.aldr@gmail.com" & echo.
+echo.
+echo.
+
+set INPUT=
+set /P INPUT=Nome do arquivo de dump: %=%
+echo.
+
+set INPUTOS=
+set /P INPUTOS=Perfil do sistema operacional: %=%
+echo.
+
+set PID=
+set /P PID=Numero do processo (PID): %=%
+echo.
+
+call :colorEcho 0E "Gerando MALFIND (trechos de codigo suspeitos)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% malfind -p %PID% > %INPUT%.malfind
+echo.
+
+call :colorEcho 0E "Gerando DLLDUMP (extracao de DLL)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% dlldump --pid=%PID% -D .\
+echo.
+
+call :colorEcho 0E "Gerando PROCDUMP (extracao de processo)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% procdump -p %PID% -D .\
+echo.
+
+call :colorEcho 0E "Gerando MEMDUMP (extracao de paginas de memoria associadas)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% memdump -p %PID% -D .\
+echo.
+
+call :colorEcho 0E "Gerando DUMPFILES (extracao de arquivos associados)" & echo.
+volatility.exe -f %INPUT% --profile=%INPUTOS% dumpfiles -n -p %PID% -D .\
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+call :colorEcho 0A " .d88888b.  888    d8P  " & echo.
+call :colorEcho 0A "d88P   Y88b 888   d8P   " & echo.
+call :colorEcho 0A "888     888 888  d8P    " & echo.
+call :colorEcho 0A "888     888 888d88K     " & echo.
+call :colorEcho 0A "888     888 8888888b    " & echo.
+call :colorEcho 0A "888     888 888  Y88b   " & echo.
+call :colorEcho 0A "Y88b. .d88P 888   Y88b  " & echo.
+call :colorEcho 0A "  Y88888P   888    Y88b " & echo.
+echo.
+echo.
+echo.
+echo.
+pause
+exit
+
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
